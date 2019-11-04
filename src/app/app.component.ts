a1dcfede93206e9485fb84673bbefcede84f5e4d
Oleksandr Poshtaruk <kievsash@gmail.com>
2019-11-04 16:46:18 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DevModeRedirectIssue';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    const url = 'http://www.mocky.io/v2/5dc036f23100006d73be3f42';
+
+    this.http.get(url).subscribe({
+      next: (data) => {
+        console.log('SUCCESS', data);
+      },
+      error: (err) => console.warn('ERROR', err),
+      complete: () => console.log('complete '),
+    });
+  }
 }
